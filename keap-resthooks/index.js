cbb8b4461c7531b2437210d91c06a95283ba5206
omaralmnt <omaralmnt@gmail.com>
2025-08-14 15:38:03 -0400

    verification

--- keap-resthooks/index.js
+++ keap-resthooks/index.js
@@ -1,13 +1,13 @@
-explorts.keapHook = (req, res) => {
+exports.keapHook = (req, res) => {
   console.log("Webhook recibido:", req.body);
 
   const hookSecret = req.header("X-Hook-Secret");
 
   if (hookSecret) {
     res.set("X-Hook-Secret", hookSecret);
-    return res.status(200).send("OK - verification");
+    return res.status(400).send("OK - verification");
   }
 
   console.log("Datos del webhook:", req.body);
-  res.status(200).send("OK");
+  res.status(400).send("OK");
 };
